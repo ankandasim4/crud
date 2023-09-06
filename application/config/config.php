0d10536802a5a4c983032e83cafae57ef5afbb9b
Ankan Das <ankanim4@gmail.com>
2023-09-06 14:09:26 +0530

    configuration updated

--- application/config/config.php
+++ application/config/config.php
@@ -23,6 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
+// Replace your domain name here after deployment
 $config['base_url'] = 'http://localhost/crud';
 
 /*
